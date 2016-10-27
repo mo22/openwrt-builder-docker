a4d8ff7612af2f4e823a4779cd49727548c08d7c
Moritz Moeller <mm@mxs.de>
2016-10-27 12:24:08 +0200

    ..

--- docker.sh
+++ docker.sh
@@ -3,31 +3,26 @@ set -eo pipefail
 
 docker build --tag openwrt-compile:latest .
 
-mkdir -p configdir dl bin
-
-cp "$2" configdir/.config
+mkdir -p dl bin
 
 if [ "$1" == "menuconfig" ]; then
     docker run --interactive --tty --rm \
-        --volume "$( realpath ./configdir )":/openwrt/configdir \
+        --volume "$( realpath . )":/openwrt/configdir \
         openwrt-compile:latest "make menuconfig V=s"
 
 elif [ "$1" == "build" ]; then
     docker run --interactive --tty --rm \
         --volume "$( realpath ./bin )":/openwrt/bin \
         --volume "$( realpath ./dl )":/openwrt/dl \
-        --volume "$( realpath ./configdir )":/openwrt/configdir \
+        --volume "$( realpath . )":/openwrt/configdir \
         openwrt-compile:latest "make defconfig && make V=s -j"
 
 elif [ "$1" == "build1" ]; then
     docker run --interactive --tty --rm \
         --volume "$( realpath ./bin )":/openwrt/bin \
         --volume "$( realpath ./dl )":/openwrt/dl \
-        --volume "$( realpath ./configdir )":/openwrt/configdir \
+        --volume "$( realpath . )":/openwrt/configdir \
         openwrt-compile:latest "make defconfig && make V=s -j1"
 
 fi
-
-cp configdir/.config "$2"
-
 
